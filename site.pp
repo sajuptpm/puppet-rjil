b71aedca4e1b3b24e0d1c4e261b81f1dff4b6f9b
Dan Bode <bodepd@gmail.com>
2015-04-27 11:55:03 -0700

    [bode] Always log keystone-manage db_sync
    b/c it is returning true, but failing.

--- site.pp
+++ site.pp
@@ -118,6 +118,9 @@ node /^ocdb\d+/ {
   include rjil::nova::controller
   include rjil::openstack_zeromq
   include rjil::openstack_objects
+  Exec <| title == 'keystone-manage db_sync' |> {
+    logoutput => true
+  }
 }
 
 #
