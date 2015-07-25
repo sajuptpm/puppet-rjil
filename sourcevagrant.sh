d6e5de530602ed329a92d95f4bb6cf5326b85c1d
Saju Madhavan <sajuptpm@gmail.com>
2015-07-25 16:13:39 +0000

    [vagrant] created new env file hiera/data/env/nwdevenv-vagrant.yaml

--- sourcevagrant.sh
+++ sourcevagrant.sh
@@ -1,5 +1,5 @@
 export layout=nwdevlayout-vagrant
-export env=nwdevenv
+export env=nwdevenv-vagrant
 export map=vagrant-vbox
 export http_proxy='http://10.140.192.30:10000/'
 export https_proxy='http://10.140.192.30:10000/'
