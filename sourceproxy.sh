620d213fc64448e89850e99b5c3a1f1290390672
Saju Madhavan <sajuptpm@gmail.com>
2015-10-14 06:15:39 +0000

    [vagrant] updated proxy infos

--- sourceproxy.sh
+++ sourceproxy.sh
@@ -1,5 +1,3 @@
-export layout=nwdevlayout-vagrant
-export env=nwdevenv-vagrant
-export map=vagrant-vbox
+export no_proxy='127.0.0.1,169.254.169.254,localhost'
 export http_proxy='http://10.140.192.24:10000/'
 export https_proxy='http://10.140.192.24:10000/'
